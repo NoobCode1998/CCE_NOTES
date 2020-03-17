6befaf89a94629c187c73f1c171e09e0cbdb8bf7
CCE NOTES <jacobranjit000@gmail.com>
2020-03-17 13:49:46 +0530

    recursion 1.0

---
+++ app/src/main/java/com/jcupzz/ccenotes/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.jcupzz.ccenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Layout;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2500);
+
+
+
+
+    }
+}
