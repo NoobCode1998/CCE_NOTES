934f16e7e0c2dfde2bb93964004b4a0a7569b5f7
CCE NOTES <jacobranjit000@gmail.com>
2020-03-17 18:00:16 +0530

    recursion 1.4

---
+++ app/src/main/java/com/jcupzz/ccenotes/StudentTeacherCategory.java
@@ -0,0 +1,37 @@
+package com.jcupzz.ccenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class StudentTeacherCategory extends AppCompatActivity {
+ImageButton stu_btn,teachers_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_teacher_category);
+
+        stu_btn = findViewById(R.id.student_category_btn);
+        teachers_btn=findViewById(R.id.teacher_category_btn);
+
+        stu_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StudentTeacherCategory.this,StudentDetailsCategory.class);
+                startActivity(intent);
+            }
+        });
+
+        teachers_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StudentTeacherCategory.this,TeacherDetailsCategory.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/jcupzz/ccenotes/TeacherDetailsCategory.java
@@ -0,0 +1,14 @@
+package com.jcupzz.ccenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherDetailsCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_details_category);
+    }
+}
