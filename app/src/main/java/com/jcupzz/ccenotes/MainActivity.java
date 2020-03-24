1d79bfbbb21a9d3a4ba18dac0374199ecadd6ab8
CCE NOTES <jacobranjit000@gmail.com>
2020-03-24 20:57:26 +0530

    added FIREBASEEEEE

--- app/src/main/java/com/jcupzz/ccenotes/MainActivity.java
+++ app/src/main/java/com/jcupzz/ccenotes/MainActivity.java
@@ -1,18 +1,97 @@
 package com.jcupzz.ccenotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends StudentDetailsCategory {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.model.Document;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
+    RecyclerView mRecyclerView;
+    ArrayList<DownModel> downModelArrayList = new ArrayList<>();
+    MyAdapter myAdapter;
+    String var;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    Intent intent = getIntent();
+    Toast.makeText(MainActivity.this,var,Toast.LENGTH_SHORT).show();
+
+        setUpFB();
+        setUpRV();
+        dataFromFirebase();
 
 
+
+    }
+
+    private void dataFromFirebase() {
+        if(downModelArrayList.size()>0)
+            downModelArrayList.clear();
+
+        //db=FirebaseFirestore.getInstance();
+
+
+db.collection("Physics")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for(DocumentSnapshot documentSnapshot: task.getResult()) {
+
+                            DownModel downModel= new DownModel(documentSnapshot.getString("name"),
+                                    documentSnapshot.getString("link"));
+                            downModelArrayList.add(downModel);
+
+                        }
+
+                        myAdapter= new MyAdapter(MainActivity.this,downModelArrayList);
+                        mRecyclerView.setAdapter(myAdapter);
+                    }
+                })
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error ;-.-;", Toast.LENGTH_SHORT).show();
+                    }
+                })
+        ;
+
+
+    }
+
+    private void setUpFB(){
+        db=FirebaseFirestore.getInstance();
+    }
+
+    private void setUpRV(){
+        mRecyclerView= findViewById(R.id.recycle);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/jcupzz/ccenotes/STwoSubjects.java
+++ app/src/main/java/com/jcupzz/ccenotes/STwoSubjects.java
@@ -2,13 +2,75 @@ package com.jcupzz.ccenotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class STwoSubjects extends AppCompatActivity {
+public class STwoSubjects extends AppCompatActivity implements View.OnClickListener{
+
+    Button s2_btn_physics;
+    Button s2_btn_chemistry;
+    Button s2_btn_graphics;
+    Button s2_btn_basics_of_mechanics;
+    Button s2_btn_basics_of_electrical;
+    Button s2_btn_basics_of_electronics;
+    Button s2_btn_mathematics;
+    Button s2_btn_cs;
+    Button s2_btn_basics_of_mechanical;
+    Button s2_btn_basics_of_civil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stwo_subjects);
+
+        //start variables
+         s2_btn_physics = findViewById(R.id.s2_btn_physics);
+        s2_btn_chemistry = findViewById(R.id.s2_btn_chemistry);
+        s2_btn_graphics = findViewById(R.id.s2_btn_graphics);
+        s2_btn_basics_of_mechanics = findViewById(R.id.s2_btn_basics_of_mechanics);
+        s2_btn_basics_of_electrical = findViewById(R.id.s2_btn_basics_of_electrical);
+        s2_btn_basics_of_electronics = findViewById(R.id.s2_btn_basics_of_electronics);
+        s2_btn_mathematics = findViewById(R.id.s2_btn_mathematics);
+        s2_btn_cs = findViewById(R.id.s2_btn_cs);
+        s2_btn_basics_of_mechanical = findViewById(R.id.s2_btn_basics_of_mechanical);
+        s2_btn_basics_of_civil = findViewById(R.id.s2_btn_basics_of_civil);
+        //end variables
+        //OnClickListenerFunction Starts:-
+        s2_btn_cs.setOnClickListener(this);
+        s2_btn_basics_of_civil.setOnClickListener(this);
+        s2_btn_basics_of_mechanical.setOnClickListener(this);
+        s2_btn_basics_of_mechanics.setOnClickListener(this);
+        s2_btn_basics_of_electronics.setOnClickListener(this);
+        s2_btn_basics_of_electrical.setOnClickListener(this);
+        s2_btn_physics.setOnClickListener(this);
+        s2_btn_chemistry.setOnClickListener(this);
+        s2_btn_mathematics.setOnClickListener(this);
+        s2_btn_graphics.setOnClickListener(this);
+
+
+
     }
-}
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(STwoSubjects.this,MainActivity.class);
+        startActivity(intent);
+        switch (v.getId()) {
+            case R.id.s2_btn_physics:
+                intent.putExtra("getphy", "Physics");
+                break;
+            case R.id.s2_btn_chemistry:
+                intent.putExtra("getchem", "Chemistry");
+                break;
+            case R.id.s2_btn_graphics:
+                intent.putExtra("getgraphics", "Graphics");
+                break;
+        }
+
+        }
+
+    }
+
