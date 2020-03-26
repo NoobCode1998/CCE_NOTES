71abf7304afa092a98281f3d6569db920f30bba6
CCE NOTES <jacobranjit000@gmail.com>
2020-03-26 17:20:00 +0530

    Finally decided for cs department only!!!! current status while committing this file: Finished S4; Same Logic for S6 and S8; Going to delete S4; Making it for only cs Department;

--- app/src/main/java/com/jcupzz/ccenotes/SSixSubjects.java
+++ app/src/main/java/com/jcupzz/ccenotes/SSixSubjects.java
@@ -2,13 +2,27 @@ package com.jcupzz.ccenotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import static com.jcupzz.ccenotes.StudentBranchCategory.j;
+import static com.jcupzz.ccenotes.StudentDetailsCategory.i;
 
 public class SSixSubjects extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ssix_subjects);
+
+
+
+
+
     }
 }
