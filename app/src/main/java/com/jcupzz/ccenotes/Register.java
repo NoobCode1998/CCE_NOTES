d2d23b48e59484e19148aaec3fd598a3b36c960c
NoobCode1998 <cyril199897@gmail.com>
2020-04-05 02:36:12 +0530

    polishing up toast messages

--- app/src/main/java/com/jcupzz/ccenotes/Register.java
+++ app/src/main/java/com/jcupzz/ccenotes/Register.java
@@ -38,17 +38,17 @@ import java.util.Map;
 
 public class Register extends AppCompatActivity {
     public static final String TAG = "TAG";
-    EditText mEmail,mPassword,mAccess;
+    EditText mEmail, mPassword, mAccess;
     Button mRegisterBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
-    String userID;
+    String userID, staffAccess, StdAccess;
 
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     //FirebaseDatabase db = FirebaseDatabase.getInstance();
-    String staff="-1";
+    String staff = "-1";
 
 
     @Override
@@ -56,17 +56,17 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mEmail      = findViewById(R.id.Email);
-        mPassword   = findViewById(R.id.password);
-        mAccess      = findViewById(R.id.access);
-        mRegisterBtn= findViewById(R.id.registerBtn);
-        mLoginBtn   = findViewById(R.id.createText);
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.password);
+        mAccess = findViewById(R.id.access);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        mLoginBtn = findViewById(R.id.createText);
 
         fAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
 
-        if(fAuth.getCurrentUser() != null){
-            startActivity(new Intent(getApplicationContext(),StudentDetailsCategory.class));
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), StudentDetailsCategory.class));
             finish();
         }
 
@@ -76,38 +76,56 @@ public class Register extends AppCompatActivity {
             public void onClick(View v) {
                 final String email = mEmail.getText().toString().trim();
                 final String password = mPassword.getText().toString().trim();
-                final String Access    = mAccess.getText().toString();
+                final String Access = mAccess.getText().toString();
 
 //                if(!((Access.equals("A1B2C4"))||Access.equals("12345"))){
 //                    mAccess.setError("the access code entered isnt for staff");
 //                    staff="1";
 //                    return;
 //                }
-                switch (Access){
+
+                DocumentReference docRef = db.collection("roles").document("AccessCode");
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document != null) {
+                                staffAccess = (document.getString("StaffCCE"));
+                                StdAccess = (document.getString("StudentCCE"));
+
+
+                            }
+                        }
+                    }
+                });
+
+
+                switch (Access) {
                     case "1234":
-                        staff="0";
+                        staff = "0";
                         break;
                     case "7894":
-                        staff="1";
+                        staff = "1";
                         break;
-                        default:
-                            mAccess.setError("invalid access code");
-                            return;
+                    default:
+                        mAccess.setError("invalid access code");
+                        return;
 
                 }
 
 
-                if(TextUtils.isEmpty(email)){
+                if (TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is Required.");
                     return;
                 }
 
-                if(TextUtils.isEmpty(password)){
+                if (TextUtils.isEmpty(password)) {
                     mPassword.setError("Password is Required.");
                     return;
                 }
 
-                if(password.length() < 6){
+                if (password.length() < 6) {
                     mPassword.setError("Password Must be >= 6 Characters");
                     return;
                 }
@@ -116,54 +134,47 @@ public class Register extends AppCompatActivity {
 
                 // register the user in firebase
 
-                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-
+                        if (task.isSuccessful()) {
 
 
                             Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
                             userID = fAuth.getCurrentUser().getUid();
 
 
-
-
                             /////cp
 
-                            role roles = new role(userID,email,staff,password);
+                            role roles = new role(userID, email, staff, password);
 
                             db.collection("roles")
                                     .document(email).set(roles)
                                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                                         @Override
                                         public void onSuccess(Void aVoid) {
-                                            Toast.makeText(getApplicationContext(),"Successfully Registered",Toast.LENGTH_SHORT).show();
-                                            if(staff.equals("1"))
-                                                Toast.makeText(getApplicationContext(),"Registered as staff",Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(getApplicationContext(), "Successfully Registered", Toast.LENGTH_SHORT).show();
+                                            if (staff.equals("1"))
+                                                Toast.makeText(getApplicationContext(), "Registered as staff", Toast.LENGTH_SHORT).show();
                                             else
-                                                Toast.makeText(getApplicationContext(),"Registered as student",Toast.LENGTH_SHORT).show();
-                                           // Intent intentd = new Intent(UploadActivity.this,MainActivity.class);
-                                           // startActivity(intentd);
+                                                Toast.makeText(getApplicationContext(), "Registered as student", Toast.LENGTH_SHORT).show();
+                                            // Intent intentd = new Intent(UploadActivity.this,MainActivity.class);
+                                            // startActivity(intentd);
                                         }
                                     })
                                     .addOnFailureListener(new OnFailureListener() {
                                         @Override
                                         public void onFailure(@NonNull Exception e) {
-                                            Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
 
                                         }
                                     });
 
 
-
                             /////cp
 
 
-
-
-
-                            startActivity(new Intent(getApplicationContext(),Login.class));
+                            startActivity(new Intent(getApplicationContext(), Login.class));
 
                         }else {
                             Toast.makeText(Register.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
@@ -175,11 +186,12 @@ public class Register extends AppCompatActivity {
         });
 
 
-
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),Login.class));
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+
             }
         });
 
