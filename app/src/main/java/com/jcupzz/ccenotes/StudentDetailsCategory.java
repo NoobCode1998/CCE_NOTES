31376b01f4dab74f8661f44b74eeb4dd341a533b
NoobCode1998 <cyril199897@gmail.com>
2020-04-06 01:35:59 +0530

    new day new issues solved a few graphic selection removed direct login screen sign out added

--- app/src/main/java/com/jcupzz/ccenotes/StudentDetailsCategory.java
+++ app/src/main/java/com/jcupzz/ccenotes/StudentDetailsCategory.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
@@ -16,15 +16,12 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.scwang.wave.MultiWaveHeader;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class StudentDetailsCategory extends MainActivity{
+public class StudentDetailsCategory extends MainActivity  {
     public static Button btn_s2,btn_s4,btn_s6,btn_s8;
     public static int i;
-MultiWaveHeader waveHeader_sdc,waveFooter_sdc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,31 +31,20 @@ MultiWaveHeader waveHeader_sdc,waveFooter_sdc;
         btn_s4 = findViewById(R.id.img_id_s4);
         btn_s6 = findViewById(R.id.img_id_s6);
         btn_s8 = findViewById(R.id.img_id_s8);
+        SharedPreferences sharedpreferences = getSharedPreferences("loginSave",
+                MODE_PRIVATE);
 
-waveHeader_sdc=findViewById(R.id.wave_head_id);
-waveFooter_sdc=findViewById(R.id.wave_fot_id);
-
-
-        waveFooter_sdc.setVelocity(2);
-        waveFooter_sdc.setProgress(1);
-        waveFooter_sdc.isRunning();
-        waveFooter_sdc.setGradientAngle(45);
-        waveFooter_sdc.setWaveHeight(40);
-        waveFooter_sdc.setStartColor(getResources().getColor(R.color.wave_start));
-        waveFooter_sdc.setCloseColor(getResources().getColor(R.color.wave_end));
-
-        waveHeader_sdc.setVelocity(2);
-        waveHeader_sdc.setProgress(1);
-        waveHeader_sdc.isRunning();
-        waveHeader_sdc.setGradientAngle(45);
-        waveHeader_sdc.setWaveHeight(40);
-        waveHeader_sdc.setStartColor(getResources().getColor(R.color.wave_end));
-        waveHeader_sdc.setCloseColor(getResources().getColor(R.color.wave_start));
+        SharedPreferences.Editor myEdit = sharedpreferences.edit();
+        String priv = sharedpreferences.getString("staff", "");
+        if(!((priv.equals("0"))||(priv.equals("1")))) {
+            startActivity(new Intent(getApplicationContext(), Register.class));
+            finish();
+        }
 
 
 
 
-        btn_s2.setOnClickListener(new View.OnClickListener() {
+btn_s2.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
         Intent s2 = new Intent(StudentDetailsCategory.this,STwoSubjects.class);
@@ -91,9 +77,8 @@ i=4;
             }
         });
 
-       // ActionBar actionBar = getSupportActionBar();
-       // actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_gradient));
-
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_gradient));
 
 
 
