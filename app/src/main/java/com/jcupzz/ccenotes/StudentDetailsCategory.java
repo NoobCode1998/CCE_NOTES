c0998a0612add7516af2cd2adbadc73f666f7205
CCE NOTES <jacobranjit000@gmail.com>
2020-04-06 19:06:07 +0530

    COLORS BLUE 1.0

--- app/src/main/java/com/jcupzz/ccenotes/StudentDetailsCategory.java
+++ app/src/main/java/com/jcupzz/ccenotes/StudentDetailsCategory.java
@@ -16,21 +16,50 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.scwang.wave.MultiWaveHeader;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDetailsCategory extends MainActivity  {
     public static Button btn_s2,btn_s4,btn_s6,btn_s8;
     public static int i;
+    MultiWaveHeader wave_head_stc,wave_foot_stc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_details_category);
 
+        wave_head_stc = findViewById(R.id.wave_head_id);
+        wave_foot_stc = findViewById(R.id.wave_fot_id);
+
         btn_s2 = findViewById(R.id.img_id_s2);
         btn_s4 = findViewById(R.id.img_id_s4);
         btn_s6 = findViewById(R.id.img_id_s6);
         btn_s8 = findViewById(R.id.img_id_s8);
+
+        wave_foot_stc.setVelocity(3);
+        wave_foot_stc.setProgress(1);
+        wave_foot_stc.isRunning();
+        wave_foot_stc.setGradientAngle(45);
+        wave_foot_stc.setWaveHeight(40);
+        wave_foot_stc.setStartColor(getResources().getColor(R.color.wave_start));
+        wave_foot_stc.setCloseColor(getResources().getColor(R.color.wave_end));
+
+        wave_head_stc.setVelocity(3);
+        wave_head_stc.setProgress(1);
+        wave_head_stc.isRunning();
+        wave_head_stc.setGradientAngle(45);
+        wave_head_stc.setWaveHeight(40);
+        wave_head_stc.setStartColor(getResources().getColor(R.color.wave_start));
+        wave_head_stc.setCloseColor(getResources().getColor(R.color.wave_end));
+
+
+
+
+
+
+
         SharedPreferences sharedpreferences = getSharedPreferences("loginSave",
                 MODE_PRIVATE);
 
@@ -77,8 +106,6 @@ i=4;
             }
         });
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_gradient));
 
 
 
